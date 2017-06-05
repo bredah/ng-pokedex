78df69e062ebe3b4d7fee2c5d8a6d553d3bd16b7
Shailesh Gavathe <sgavathe@gmail.com>
2017-06-05 16:24:27 -0400

    Update app.po.ts

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,1 @@
-import { browser, element, by } from 'protractor';
 
-export class NgPokedexPage {
-  navigateTo() {
-    return browser.get('/home');
-  }
-
-  getHeadingText() {
-    return element(by.css('app-root h1')).getText();
-  }
-}
